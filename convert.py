dd1a7ba1ac69730e1e92195fbbb98ef71cddea4c
lunax <lunax@mlunax.com>
2022-06-06 08:09:01 +0200

    add meta utf8 to html

--- convert.py
+++ convert.py
@@ -3,6 +3,7 @@ import markdown
 with open('CHANGELOG.md', 'r',encoding='utf8') as f:
     text = f.read()
     html = markdown.markdown(text)
+    html = html + "<meta charset=\"utf-8\">"
 
 with open('CHANGELOG.html', 'w',encoding='utf8') as f:
     f.write(html)

9198125d8d2c59a8de4e011ad2851443e6215226
lunax <lunax@mlunax.com>
2022-06-06 07:45:28 +0200

    change encoding to UTF-8

--- convert.py
+++ convert.py
@@ -1,9 +1,9 @@
 import markdown
 
-with open('CHANGELOG.md', 'r') as f:
+with open('CHANGELOG.md', 'r',encoding='utf8') as f:
     text = f.read()
     html = markdown.markdown(text)
 
-with open('CHANGELOG.html', 'w') as f:
+with open('CHANGELOG.html', 'w',encoding='utf8') as f:
     f.write(html)
 
